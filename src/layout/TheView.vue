f2228c015deaded9a7c52cdfecefcd499777324d
순진 <jds920805@gmail.com>
2025-09-18 23:09:53 +0900

    first commit

---
+++ src/layout/TheView.vue
@@ -0,0 +1,11 @@
+<template>
+  <main>
+    <div class="container py-5">
+      <h1>Hello vue3</h1>
+    </div>
+  </main>
+</template>
+
+<script setup></script>
+
+<style lang="scss" scoped></style>
